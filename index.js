d7f2a7717577c54d4056fd62a6001623b4030582
cyberkyojin <cyberkyojin@gmail.com>
2021-09-26 22:01:04 -0300

    ;-;

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 function available(url) {
-	return fetch(url)
+	fetch(url)
 		.then((response) => {
 			let r = response.ok == true ? "accessible page" : "page not accessible";
 			console.log(r);
